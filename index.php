30ce90af7e3f0f6be383beb1303ae1b5786fdeeb
Bruno Santana <brunomoraes.bm.bm@gmail.com>
2023-03-29 22:05:57 -0300

    Project10 - Agência de Marketing

---
+++ index.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link href="css/template.css" type="text/css" rel="stylesheet"/>
+        <title>Project 10 | Agency</title>
+         <!----------Tags SEO Google--------->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="keywords" content="bruno s. moraes, bruno, moraes, bruno santana, descomplica university, descomplica, university, student, front-end, back-end, fullstack,  web, website, computer, software, developer, engineer, designer, co-op" />
+        <meta name="author" content="Bruno Moraes" />
+        <meta name="description" content="Fullstack Developer Jr,  Enthusiast, and everything in between." />
+        <meta property="og:description" content="Fullstack Developer, Enthusiast, and everything in between." />
+        <meta http-equiv="X-UA-Compatible" content="IE-edge"/>
+        <meta name="author" content="Bruno Santana"/>
+        <!----------Tags SEO Facebook--------->
+        <meta property="og:url"                content="" />
+        <meta property="og:type"               content="" />
+        <meta property="og:title"              content="" />
+        <meta prHow much does culture influence creative thinking?" />
+        <meta property="og:image"              content="" />
+        <!----------Tags SEO Twitter--------->
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="" />
+        <meta name="twitter:creator" content="" />
+        <meta property="og:url" content="" />
+        <meta property="og:title" content="A Twitter for My Sister" />
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="" />
+        <!----------Links de Style--------->
+        <link href="images/logo.png" type="image-x/png" rel="shortcut icon"/>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div class="menu-topo">
+                    <div class="logo">Bdeveloper</div>
+                    <nav class="desktop">
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">Contacts</a></li>
+                            <li class="btn-nav"><a href="#">I want a budget</a></li>
+                        </ul>
+                    </nav><!---desktop-->
+                    <nav class="mobile">
+                        <h3><i class="fa fa-bars"></i></h3>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Portfolio</a></li>
+                            <li><a href="#">Contacts</a></li>
+                            <li class="btn-nav"><a href="#">I want a budget</a></li>
+                        </ul>
+                    </nav><!---mobile--->
+                    <div class="clear"></div>
+                </div><!---menu-topo-->
+
+                <div class="chamada-header">
+                    <h2>You sell more<br>with web automation</h2>
+                    <p>We create marketing solutions for your company to sell more, in the best way.</p>
+                    <form>
+                        <input type="email" name="email" required placeholder="Seu email...">
+                        <input type="submit" name="acao" value="I want to know more!">
+                    </form>
+                </div><!---chamada-header-->
+            </div><!---container-->
+        </header>
+
+        <section class="descricao-bdeveloper">
+            <div class="container">
+                <p>With years of experience in web development and marketing automation, <b>Bdeveloper</b> brings solutions that help your team sell more in both B2C and B2B segments.</p>
+            </div><!---container--->
+        </section><!---descrição-bdeveloper--->
+
+        <section class="descricao-bdeveloper2">
+            <div class="container">
+                <div class="texto-descricao">
+                    <h3>We are a digital agency. We work on its positioning, content and aesthetics, always focused on conversion.</h3>
+                    <p>Chat with our team today and create a marketing plan for your business!</p>
+                </div><!---texto-descrição--->
+                <img src="images/mockup1.jpg" alt="bdeveloper picture" title="bdeveloper job" />
+                <div class="clear"></div>
+            </div><!---container--->
+        </section><!---descrição-bdeveloper2-->
+
+        <section class="descricao-bdeveloper3">
+            <div class="container">
+                <div class="box-depoimento">
+                    <p>“We have Bdeveloper as a strategic partner in our digital platform planning. Always bringing us innovative solutions suited to our needs and those of our customers. Highly recommend!”</p>
+                    <br/>
+				    <p><b>Rogério - Voodoo Branded Content</b></p>
+                </div><!---box-depoimentos-->
+            </div><!--container--->
+        </section><!---descrição-bdeveloper3-->
+
+        <section class="servicos">
+            <div class="container">
+                <div class="title-servicos">
+                    <h3>How can we help your company<br /><i class=" fa fa-angle-down"></i></h3>
+                </div><!---title-servicos-->
+
+                <div class="flex-servicos">
+
+                    <div class="space-servico">
+                        <div class="box-servico">
+                            <img src="images/img1.jpg"/>
+                            <div class="box-servico-wraper">
+                                <h2>E-mail Marketing</h2>
+                                <p>Learn how to use email marketing in your company to leverage your sales in an automated way.</p>
+                                <a href="#">Know more!</a>
+                            </div><!---box-serviços-wraper-->
+                        </div><!---box-servico--->
+                    </div><!---space-servicos-->
+
+                    <div class="space-servico">
+                        <div class="box-servico">
+                            <img src="images/img2.jpg"/>
+                            <div class="box-servico-wraper">
+                                <h2>Sales Pages</h2>
+                                <p>Learn how to use email marketing in your company to leverage your sales in an automated way.</p>
+                                <a href="#">Know more!</a>
+                            </div><!---box-serviços-wraper-->
+                        </div><!---box-servico--->
+                    </div><!---space-servicos-->
+
+                    <div class="space-servico">
+                        <div class="box-servico">
+                            <img src="images/img3.jpg"/>
+                            <div class="box-servico-wraper">
+                                <h2>Capture and Signup Pages</h2>
+                                <p>Learn how to use email marketing in your company to leverage your sales in an automated way.</p>
+                                <a href="#">Know more!</a>
+                            </div><!---box-serviços-wraper-->
+                        </div><!---box-servico--->
+                    </div><!---space-servicos-->
+
+                    <div class="space-servico">
+                        <div class="box-servico">
+                            <img src="images/img4.jpg"/>
+                            <div class="box-servico-wraper">
+                                <h2>SEO & Content Marketing</h2>
+                                <p>Learn how to use email marketing in your company to leverage your sales in an automated way.</p>
+                                <a href="#">Know more!</a>
+                            </div><!---box-serviços-wraper-->
+                        </div><!---box-servico--->
+                    </div><!---space-servicos-->
+
+                    <div class="space-servico">
+                        <div class="box-servico">
+                            <img src="images/img5.jpg"/>
+                            <div class="box-servico-wraper">
+                                <h2>ADWords & Facebook ADS</h2>
+                                <p>Learn how to use email marketing in your company to leverage your sales in an automated way.</p>
+                                <a href="#">Know more!</a>
+                            </div><!---box-serviços-wraper-->
+                        </div><!---box-servico--->
+                    </div><!---space-servicos-->
+
+                    <div class="space-servico">
+                        <div class="box-servico">
+                            <img src="images/img6.jpg"/>
+                            <div class="box-servico-wraper">
+                                <h2>Digital Marketing Consulting</h2>
+                                <p>Learn how to use email marketing in your company to leverage your sales in an automated way.</p>
+                                <a href="#">Know more!</a>
+                            </div><!---box-serviços-wraper-->
+                        </div><!---box-servico--->
+                    </div><!---space-servicos-->
+
+                </div><!--flex-servicos--->
+            </div><!---container--->
+        </section><!---servicos--->
+
+        <section class="portfolio">
+            <div class="container">
+                <h2>Some companies we had the opportunity to work with.</h2>
+                <div class="empresas">
+
+                    <div class="empresa-single">
+                        <img src="images/bianchini.png"/>
+                    </div><!--empresa-single-->
+
+                    <div class="empresa-single">
+                        <img src="images/rancho.png"/>
+                    </div><!--empresa-single-->
+
+                    <div class="empresa-single">
+                        <img src="images/hoepcke.png"/>
+                    </div><!--empresa-single-->
+
+                    <div class="empresa-single">
+                        <img src="images/monguilhott.png"/>
+                    </div><!--empresa-single-->
+
+                </div><!---empresas--->
+            </div><!---container--->
+        </section><!---portfolio--->
+
+        <section class="formulario">
+            <div class="container">
+                <h2>A budget or any questions?</h2>
+                <form>
+                    <input type="text" name="nome" placeholder="Your name..." required>
+                    <input type="email" name="emai" placeholder="Your emai..." required>
+                    <input type="telefone" name="telefone" placeholder="Your Telephone..." required>
+                    <textarea name="Your Message..." required></textarea>
+                    <input type="submit" name="acao" value="Send"/>
+                </form>
+            </div><!--container-->
+        </section><!---formulario-->
+        
+        <footer>
+            <div class="container">
+
+                <div class="box-footer">
+                    <h2>Address</h2>
+                    <p>Avendia Alberico Diniz 1882 - Jardim Sulacap - Rio de Janeiro</p>
+                </div><!---box-footer-->
+
+                <div class="box-footer">
+                    <h2>Telephone</h2>
+                    <p>+55(21)9925665588</p>
+                </div><!---box-footer-->
+
+                <div class="box-footer">
+                    <h2>Receive our news</h2>
+                    <form>
+					    <input placeholder="E-mail..." type="text" /><button type="submit"><i class="fa fa-paper-plane-o"></i></button>
+				    </form>
+                </div><!---box-footer-->
+
+                <div class="clear"></div>
+            </div><!---container-->
+            <div class="claer"></div>
+        </footer>
+    </body>
+    <script src="js/jquery.js"></script>
+    <script>
+        $('nav.mobile h3').click(function(){
+            $('nav.mobile').find('ul').slideToggle();
+        })
+    </script>
+</html>
